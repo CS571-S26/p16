7b9a9c2ed082faed90517e7d42b2362999c0457c
Raghu <raghu@Rohans-MacBook-Air.local>
2026-04-08 18:04:40 -0500

    fix routing for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Neighborhoods from './pages/Neighborhoods';
 import NavigationBar from './components/NavBar'; 
